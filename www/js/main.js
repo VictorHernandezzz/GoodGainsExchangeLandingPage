84a5b9778969855088301599ecba63df255a5b65
Bikutā ビクター <anthosedits@gmail.com>
2022-04-13 14:50:57 -0400

    Updated Landing Page
    I replaced the home slider image with a js script that checks device width and plays either a sick video for desktops, and a cool gif for mobile phones.

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 	"use strict"; // start of use strict
-	var video = document.getElementById("myVideo");
 	/*==============================
 	Mobile navigation
 	==============================*/
